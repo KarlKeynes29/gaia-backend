6501d6529b67507e87b103e1a4a596faa3cc69b0
Karl Keynes Abid <karlkeynes.abid@jti.com.ph>
2026-09-10 17:38:53 +0800

    Add lines of code for product controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,5 +3,26 @@ import { Op, Order, ModelStatic, Model } from 'sequelize';
 import { Game, MerchItem } from '../../src/models/index';
 
 const getModel = (type: string): ModelStatic<Model> | null => {
-	
+    const properType = type.toLowerCase();
+    if (properType === 'game') return Game as unknown as ModelStatic<Model>;
+    if (properType === 'merch') return MerchItem as unknown as ModelStatic<Model>;
+    return null;
+};
+
+interface FilterQueryParams {
+    searchValue?: string;
+    genre?: string;
+    source?: string;
+    is_featured?: string;
+    is_available?: string;
+    priceFrom?: string;
+    priceTo?: string;
+    page?: string;
+    limit?: string;
+    sortBy?: string;
 }
+
+export const getAllProducts = async (req: Request<{type: string}, {}, {}, FilterQueryParams>, res: Response) => {
+
+}
+const TargetModel = getModel('game');
