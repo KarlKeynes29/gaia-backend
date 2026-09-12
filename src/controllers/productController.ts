6554fd96418406bf24851aede30ca8b561e09e49
karlkeynes <keynesabid@gmail.com>
2026-09-12 18:05:03 +0800

    Studying this while coding it is sometimes frustrating :D

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,5 +3,7 @@ import { Op, Order, ModelStatic, Model } from 'sequelize';
 import { Game, MerchItem } from '../../src/models/index';
 
 const getModel = (type: string): ModelStatic<Model> | null => {
+	const normalizedType = type.toLowerCase();
+	if (normalizedType === 'game') return Game as unknown as ModelStatic<Model>;
 	
 }
