6d01e53e7d9ceb24e5020b1bb103ed6d67386657
Karl Keynes Abid <karlkeynes.abid@jti.com.ph>
2026-09-14 17:17:18 +0800

    Refactoring backend logic for fetching, merging previously separate controllers for Game and MerchItem.

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -22,8 +22,72 @@ interface FilterQueryParams {
     sortBy?: string;
 }
 
-export const getAllProducts = async (req: Request<{type: string}, {}, {}, FilterQueryParams>, res: Response) => {
+export const getAllProducts = async (
+    req: Request<{ type: string }, {}, {}, FilterQueryParams>,
+    res: Response
+) => {
+    const { type } = req.params;
+    const { } = req.query;
 
-}
-const TargetModel = getModel('game');
-type Blob<T> = T extends Promise(...args: any[]) => infer R ? R : never;
+    const targetModel = getModel(type);
+
+    // Currently just Game and MerchItem
+    if (!targetModel) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid product type!',
+        });
+    }
+
+    try {
+        const products = await targetModel.findAll();
+
+        if (!products) {
+            return res.status(200).json({
+                success: true,
+                message: 'No products found.',
+                data: [],
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: 'Products successfully fetched!',
+            data: products,
+        });
+    } catch (error) {
+        console.error(`Error in fetching the products.`, error);
+        return res.status(500).json({
+            success: false,
+            message: `Failed to retrieve the products due to a server error.`,
+        });
+    }
+};
+
+const getProductById = async (req: Request<{ type: string, id: string }>, res: Response) => {
+    const { type, id } = req.params;
+    const targetModel = getModel(type);
+
+    if (!targetModel) {
+        return res.status(400).json({
+            success: false,
+            message: 'Invalid product type!',
+        });
+    }
+
+    try {
+        const product = await targetModel.findByPk(id);
+        if (!product) {
+            return res.status(404).json({
+               success: false,
+               message: `Product with ID: ${id} not found.`
+            });
+        }
+        return res.status(200).json({
+            success: true,
+            message: 'Product succesfully fetched!',
+            data: product,
+        });
+    } catch (error) {
+
+    }
+};
